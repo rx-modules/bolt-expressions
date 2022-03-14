82c3ff50c390bdee6069a12b27239d8131c8e706
rx97 <rx97>
2022-03-14 14:34:21 -0700

    adjustments

--- pointers/operations.py
+++ pointers/operations.py
@@ -1,29 +1,29 @@
 from dataclasses import dataclass, field
-from typing import Union
+from typing import Optional, Union, List
 from beet import Context
 
-GenericValue = Union["Score", int]
+GenericValue = Union["ScoreSource", int]
 
 @dataclass
 class ExpressionNode:    
     def __add__(self, other: "ExpressionNode"):
-        print(f"Adding {self} to {other}")
+        # print(f"Adding {self} by {other}")
         return Add(self, other)
         
     def __sub__(self, other: "ExpressionNode"):
-        print(f"Subtracting {self} to {other}")
+        # print(f"Subtracting {self} by {other}")
         return Subtract(self, other)
         
     def __mul__(self, other: "ExpressionNode"):
-        print(f"Multiplying {self} to {other}")
+        # print(f"Multiplying {self} by {other}")
         return Multiply(self, other)
         
     def __truediv__(self, other: "ExpressionNode"):
-        print(f"Dividing {self} to {other}")
+        # print(f"Dividing {self} by {other}")
         return Divide(self, other)
         
     def __mod__(self, other: "ExpressionNode"):
-        print(f"Modulus {self} to {other}")
+        # print(f"Modulus {self} by {other}")
         return Modulus(self, other)
 
 
@@ -32,38 +32,65 @@ class ScoreSource(ExpressionNode):
     scoreholder: str
     objective: str
 
+    def __str__(self):
+        return f"{self.scoreholder} {self.objective}"
+
 @dataclass
 class Operation(ExpressionNode):
-    former: GenericValue
-    latter: GenericValue
+    former: Union["Operation", ScoreSource]
+    latter: Union["Operation", GenericValue]
 
+    operation: str = field(default="scoreboard players operation {former} {operand} {latter}", repr=False)
+    
+    def resolve(self):
+        return self.operation.format(former=self.former, operand=self.operand, latter=self.latter)
+    
+    def __post_init__(self):
+        # TODO: add score to init file
+        # TODO: use generic integer obj / something from config
+        if type(self.latter) is int:
+            self.latter = ScoreSource(f"${self.latter}", "rx.int")
+
+class Set(Operation):
+    operand: str = "="
 
 class Add(Operation):
-    pass
+    operand: str = "+="
 
 class Subtract(Operation):
-    pass
+    operand: str = "-="
 
 class Multiply(Operation):
-    pass
+    operand: str = "*="
 
 class Divide(Operation):
-    pass
+    operand: str = "/="
 
 class Modulus(Operation):
-    pass
+    operand: str = "%="
 
 
 @dataclass
 class Score:
-    ctx: Context
+    ctx: Context = field(repr=False)
     objective: str
 
     def __getitem__(self, scoreholder: str) -> ExpressionNode:
         return ScoreSource(scoreholder, self.objective)
     
-    def __setitem__(self, scoreholder: str, value: Union[ExpressionNode, GenericValue]):
-        ...
+    def __setitem__(self, scoreholder: str, value: Union[Operation, GenericValue]):
+        # print(f"Setting {self} to {value}")
+        
+        cmds = self.resolve(Set(self.__getitem__(scoreholder), value))
+        print('\n'.join(cmds))
+        print()
+
+    def resolve(self, root: Union[ExpressionNode, int]) -> List[str]:
+        if isinstance(root, Operation):
+            yield from self.resolve(root.former)
+            yield from self.resolve(root.latter)
+            yield root.resolve()
+
 
 
 # myself = Score("@s", "temp")
--- src/data/test/functions/main.mcfunction
+++ src/data/test/functions/main.mcfunction
@@ -8,12 +8,18 @@ uid = Objective("rx.uid")
 # normal set
 uid["@s"] = 5
 
+# normal set
+uid["@s"] += 5
+uid["@s"] += uid["@s"]
+
 # mul + assignment
 uid["@s"] = uid["@s"] * 5
 uid["@s"] *= 5
 
 # set from other
 uid["@s"] = uid["$current.id"]
+
+uid["my_var"] = (uid["@s"] + uid["@e"]) * 5 + uid["@s"] * 2
 
 #/ io = objectives.create("io")  # assuming generate_namespace is set to rx.pdb
 #/ io["@s"] = 5
