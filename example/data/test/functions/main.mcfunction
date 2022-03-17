367d3bdc036298c8d3eaede563445f6ce6c8a2fd
TheWii <67249660+TheWii@users.noreply.github.com>
2022-03-17 17:12:41 -0300

    Add more optimization rules

--- example/data/test/functions/main.mcfunction
+++ example/data/test/functions/main.mcfunction
@@ -4,57 +4,92 @@ tellraw @a "Smithed"
 
 Objective = ctx.inject(Scoreboard)
 
-# uid = Objective("rx.uid")
+abc = Objective("abc.main")
 
-# uid["@s"] += 10 * (2 * uid["value"] / 10)
+# "output_score_replacement" ignores the first line
+# scoreboard players operation $i0 temp = @s abc.main
+#abc["@s"] = 3 * (2 * (abc["@s"] * abc["#val"]))
 
+# fails to optimize because abc["@s"] is in the right side
+# of addition, moving the output source reference to the
+# second command
+#abc["@s"] = 3 * (abc["#val"] + abc["@s"])
 
-def minn(arg1, arg2):
-    return arg1 < arg2
-
-def maxx(arg1, arg2):
-    return arg1 > arg2
-
-smithed_damage = Objective("smithed.damage")
-
-# smithed_damage["max"] = maxx(smithed_damage["@s"], 10)
-
-# smithed_damage = Objective("smithed.damage")
-damage = smithed_damage["damage"]
-toughness = smithed_damage["toughness"]
-armor = smithed_damage["armor"]
-
-atf = (10 * armor - (400 * damage / (80 + 10 * toughness)))
-maxxed = maxx((10 * armor) / 5, atf)
-damage = damage * (250 - (minn(200, maxxed))) / 25
-
-smithed_damage = Objective("smithed.damage")
+# "output_score_replacement" thrives with this one!
+#abc["@s"] = 2 + 3 * (4 * (abc["@s"] + abc["#val"]))
 
 
-# atf = (10 * smithed_damage["armor"] - (400 * smithed_damage["damage"] / (80 + 10 * smithed_damage["toughness"])))
-# maxxed = max((10 * smithed_damage["armor"]) / 5, atf)
-# smithed_damage["damage"] = smithed_damage["damage"] * (250 - (min(200, maxxed))) / 25
 
-#/ damage = #damage * (250 - (min(200, max( 10armor ÷ 5   , #10ATF ))) / 25
+#abc["@s"] *= abc["@s"]
 
-# # normal set
-# uid["@s"] = 5
+#add = 10 * (abc["#value"] + 2)
+#abc["@s"] = add * 2
 
-# # # normal set
-# uid["@s"] += 5
-# uid["@s"] += uid["@s"]
+#abc["@s"] += 10 * (abc["#val"] + 2)
 
-# # mul + assignment
-# uid["@s"] *= 5
 
-# # # set from other
-# uid["@s"] = uid["$current.id"]
+# Noncommutative set collapsing might help here
+#abc["@s"] -= abc["#val"] + 1
 
-#uid["my_var"] = (uid["@s"] + uid["@e"]) * 5 + uid["@s"] * 2
+# Also here (Accidentaly heh)
+abc["@s"] *= abc["@s"]
 
-#/ io = objectives.create("io")  # assuming generate_namespace is set to rx.pdb
-#/ io["@s"] = 5
-#/ io["@s"] += io["@e[tag=other]"] * 2
+# Here it falls back to Commutative set collapsing
+#abc["@s"] *= (abc["@s"] + 1)
 
-# low key i kinda prefer
-#/ io = Objective("io") over objectives.create()?
+#
+ # uid = Objective("rx.uid")
+ #
+ # uid["@s"] += 10 * (2 * uid["value"] / 10)
+ #
+ #
+ #def minn(arg1, arg2):
+ #    return arg1 < arg2
+ #
+ #def maxx(arg1, arg2):
+ #    return arg1 > arg2
+ #
+ #smithed_damage = Objective("smithed.damage")
+ #
+ # smithed_damage["max"] = maxx(smithed_damage["@s"], 10)
+ #
+ ## smithed_damage = Objective("smithed.damage")
+ #damage = smithed_damage["damage"]
+ #toughness = smithed_damage["toughness"]
+ #armor = smithed_damage["armor"]
+ #
+ #atf = (10 * armor - (400 * damage / (80 + 10 * toughness)))
+ #maxxed = maxx((10 * armor) / 5, atf)
+ #damage = damage * (250 - (minn(200, maxxed))) / 25
+ #
+ #smithed_damage = Objective("smithed.damage")
+ #
+ #
+ # atf = (10 * smithed_damage["armor"] - (400 * smithed_damage["damage"] / (80 + 10 * smithed_damage["toughness"])))
+ # maxxed = max((10 * smithed_damage["armor"]) / 5, atf)
+ # smithed_damage["damage"] = smithed_damage["damage"] * (250 - (min(200, maxxed))) / 25
+ #
+ #/ damage = #damage * (250 - (min(200, max( 10armor ÷ 5   , #10ATF ))) / 25
+ #
+ # # normal set
+ # uid["@s"] = 5
+ #
+ # # # normal set
+ # uid["@s"] += 5
+ # uid["@s"] += uid["@s"]
+ #
+ # # mul + assignment
+ # uid["@s"] *= 5
+ 
+ # # # set from other
+ # uid["@s"] = uid["$current.id"]
+ 
+ #uid["my_var"] = (uid["@s"] + uid["@e"]) * 5 + uid["@s"] * 2
+ 
+ #/ io = objectives.create("io")  # assuming generate_namespace is set to rx.pdb
+ #/ io["@s"] = 5
+ #/ io["@s"] += io["@e[tag=other]"] * 2
+ 
+ # low key i kinda prefer
+ #/ io = Objective("io") over objectives.create()?
+ 
--- pointers/optimizer.py
+++ pointers/optimizer.py
@@ -1,13 +1,13 @@
 from typing import Any, Callable, Dict, Iterable, List, TYPE_CHECKING
 
 from rich import print
+from rich.pretty import pprint
 
 from .sources import ConstantScoreSource, ScoreSource, TempScoreSource
 from . import operations as op
 
 if TYPE_CHECKING:
     Rule = Callable[[Iterable[op.Operation]], None]
-
 
 class Optimizer:
     rules: List["Rule"] = []
@@ -22,7 +22,6 @@ class Optimizer:
             nodes = rule(nodes)
 
         yield from nodes
-
 
 @Optimizer.rule
 def temp_var_collapsing(nodes: Iterable["op.Operation"]):
@@ -47,6 +46,98 @@ def temp_var_collapsing(nodes: Iterable["op.Operation"]):
         else:
             yield node.__class__(get_replaced(node.former), get_replaced(node.latter))
 
+#@Optimizer.rule
+def noncommutative_set_collapsing(nodes: Iterable["op.Operation"]):
+    """
+        For noncommutative operations:
+
+        scoreboard players operation $i1 temp = @s rx.uid
+        scoreboard players operation $i1 temp -= $i0 temp
+        scoreboard players operation @s rx.uid = $i1 temp
+
+        Becomes:
+
+        scoreboard players operation @s rx.uid -= $i0 temp
+
+    """
+    ...
+
+@Optimizer.rule
+def commutative_set_collapsing(nodes: Iterable["op.Operation"]):
+    """
+        For commutative operations:
+
+        scoreboard players operation $i1 temp += $i0 temp
+        scoreboard players operation $i0 temp = $i1 temp
+
+        Becomes:
+
+        scoreboard players operation $i0 temp += $i1 temp
+    """
+    collapsed_last = False
+    prev_node = next(nodes)
+    for node in nodes:
+        #print(f"\nPrevious node is {prev_node}")
+        #print(f"Current nodes is {node}")
+        if (
+            type(prev_node) in (op.Add, op.Multiply)
+            and type(node) is op.Set
+            and prev_node.former == node.latter
+            and prev_node.latter == node.former
+        ):
+            #print("Commutative set collapsing matched!")
+            yield prev_node.__class__(
+                node.former, node.latter
+            )
+            collapsed_last = True
+        else:
+            yield prev_node
+            collapsed_last = False
+        prev_node = node
+    # If not collapsed in the last loop: yield last node
+    if not collapsed_last: yield prev_node
+
+@Optimizer.rule
+def output_score_replacement(nodes: Iterable["op.Operation"]):
+    """
+        Replace the outermost temp score by the output score.
+        If expression tree uses the output score, this rule
+        won't be applied.
+    """
+    all_nodes = list(nodes)
+    #print("[bold]Applying output score replacement on tree:[/bold]")
+    #pprint(all_nodes)
+    last_node = all_nodes[-1]
+    target_var = last_node.latter
+    output_var = last_node.former
+    can_replace = False
+    #print(f"Last node is {last_node}")
+    if len(all_nodes) > 1 and type(last_node) is op.Set:
+        for node in all_nodes[1:]: # Ignore the first Set operation
+            #print(f"Is {node.latter} equal to {output_var}? {node.latter == output_var}")
+            if node.latter == output_var: break
+        else: can_replace = True
+    #print(f"Can replace output score? {can_replace}")
+    def replace(source):
+        if source == target_var: return output_var
+        return source
+    if can_replace:
+        for node in all_nodes:
+            yield node.__class__(
+                replace(node.former),
+                replace(node.latter)
+            )
+    else: yield from all_nodes
+
+@Optimizer.rule
+def set_to_self_removal(nodes: Iterable["op.Operation"]):
+    """ Removes Set operations that have the same former and latter source"""
+    for node in nodes:
+        if type(node) is op.Set:
+            if node.former != node.latter: yield node
+        else:
+            yield node
+            
 
 @Optimizer.rule
 def constant_to_literal_replacement(nodes: Iterable["op.Operation"]) -> Iterable["op.Operation"]:
